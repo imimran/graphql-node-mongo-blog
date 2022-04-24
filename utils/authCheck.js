9d952f0b277a16d54c02b4473c732c32cbe0f9a6
imimran <engr.aih@gmail.com>
2022-04-24 21:38:03 +0600

    auth check function

---
+++ utils/authCheck.js
@@ -0,0 +1,21 @@
+const { AuthenticationError } = require('apollo-server');
+const jwt = require('jsonwebtoken')
+
+module.exports = (context) =>{
+    const authHeader = context.req.headers.authorization;
+    if(authHeader){
+        const token = authHeader.split('Bearer ')[1]
+
+        if(token){
+            try {
+               const user = jwt.verify(token, "SCREAT_KEY")
+               return user 
+            } catch (error) {
+                throw new AuthenticationError("Invalid/Expired Token")
+            }
+        }
+        throw new Error("Authentication token must be 'Bearer [token]' ")
+    }
+    throw new Error("Authentication token must be provided")
+    
+}
